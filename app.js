fdce436a5adc757e27873314b244dfe4b1b7cc01
Hazrat Bilal <hazratbilal52521@gmail.com>
2024-05-08 10:46:08 +0500

    First Commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const nav = document.querySelector("nav"),
+toggleBtn = nav.querySelector(".toggle-btn");
+
+toggleBtn.addEventListener("click", ()=>{
+    nav.classList.toggle("open")
+})
+
+function onDrag({movementY}){
+    const navStyle = window.getComputedStyle(nav),
+    navTop = parseInt(navStyle.top),
+    navHeight = parseInt(navStyle.height),
+    windHeight = window.innerHeight;
+
+    nav.style.top = navTop > 0 ? `${navStyle + movementY}px`: "1px";
+    if (navTop > windHeight - navHeight) {
+        nav.style.top = `${windHeight = navHeight}px`;
+    } 
+}
+nav.addEventListener("mousedown", ()=>{
+    nav.addEventListener("mousemove", onDrag);
+})
+nav.addEventListener("mouseup", ()=>{
+    nav.removeEventListener("mousemove",onDrag);
+})
+nav.addEventListener("mouseleave", ()=>{
+    nav.removeEventListener("mousemove", onDrag);
+})
